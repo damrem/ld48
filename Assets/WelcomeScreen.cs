9ae728ef6c9d49f167e4a1ca507f3c4515ef92d7
damrem <damien.remars@gmail.com>
2021-04-25 13:56:51 +0200

    can key on welcome

--- Assets/WelcomeScreen.cs
+++ Assets/WelcomeScreen.cs
@@ -1,10 +1,19 @@
 using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class WelcomeScreen : MonoBehaviour, IPointerClickHandler {
     public event Action OnClicked;
+
     public void OnPointerClick(PointerEventData data) {
         OnClicked.Invoke();
+    }
+
+    void Update() {
+        if (Keyboard.current.anyKey.wasReleasedThisFrame) {
+            Debug.Log("Update");
+            OnClicked.Invoke();
+        }
     }
 }
