e481acd6969514f0087794b36a279842015e2dba
damrem <damien.remars@gmail.com>
2021-04-25 10:13:51 +0200

    cam pos

--- Assets/CameraMan.cs
+++ Assets/CameraMan.cs
@@ -7,7 +7,7 @@ public class CameraMan : MonoBehaviour {
     Level Level;
     public CameraMan Init(Transform target, Level level) {
         Debug.Log("Init " + target);
-        GetComponent<Camera>().orthographicSize = level.Def.Width;
+        GetComponent<Camera>().orthographicSize = level.Def.Width + .5f;
         Target = target;
         Level = level; ;
         return this;
@@ -18,7 +18,7 @@ public class CameraMan : MonoBehaviour {
 
         var targetY = Target.position.y + 4;
         if (targetY < -Level.Def.Depth + 8) targetY = -Level.Def.Depth + 8;
-        var pos = new Vector3((float)Level.Def.Width / 2 - .5f, targetY, -10);
+        var pos = new Vector3((float)Level.Def.Width / 2 - .25f, targetY, -10);
         transform.position = pos;
     }
 }
